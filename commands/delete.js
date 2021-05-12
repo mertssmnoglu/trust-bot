8f332eeb4f1bb6382eb38ea8cebaddf211642d00
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-05-12 12:46:50 +0300

    Add delete.js for bulkdelete messages.

---
+++ commands/delete.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+module.exports.config = {
+    name: "delete",
+    aliases: ["clear", "clean"]
+}
+
+exports.run = (client, message, args) => {
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+        return message.channel.send(":warning: You can't use this command.")
+    }
+    if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) {
+        return message.channel.send(":warning: I can't delete messages. :cry:")
+    }
+    var number = args[0];
+    if (!number) {
+        return message.channel.send(`How many messages you want to delete ?`)
+    }
+    if (isNaN(number)) {
+        return message.channel.send(`Only numbers please.`)
+    }
+    if (number <= 0 || number > 100) {
+        return message.channel.send(`:person_facepalming: Use numbers between 0-100`)
+    }
+
+    message.channel.bulkDelete(number, true)
+    message.channel.send(`${number} message deleted.`).then(message => {
+        message.delete({
+            timeout: 3500
+        })
+    })
+}

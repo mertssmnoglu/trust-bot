f8f77478041e687c6cec9da76a59a0ec837d98ec
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-05-12 11:18:47 +0300

    Add casesensivity option to message event.

--- events/message.js
+++ events/message.js
@@ -1,12 +1,23 @@
 module.exports = (client, message) => {
     if (message.author.bot || message.content.indexOf(client.config.prefix) !== 0) return;
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+    const command = args.shift();
+    var caseSensitive = true
     let cmd;
-    if (client.commands.has(command)) {
-        cmd = client.commands.get(command);
-    } else if (client.aliases.has(command)) {
-        cmd = client.commands.get(client.aliases.get(command));
+    if (caseSensitive) {
+        if (client.commands.has(command)) {
+            cmd = client.commands.get(command);
+        }
+    } else {
+        if (client.commands.has(command)) {
+            cmd = client.commands.get(command);
+        } else if (client.commands.has(command.toLowerCase())) {
+            cmd = client.commands.get(command.toLowerCase());
+        } else if (client.aliases.has(command)) {
+            cmd = client.commands.get(client.aliases.get(command));
+        } else if (client.aliases.has(command.toLowerCase())) {
+            cmd = client.commands.get(client.aliases.get(command.toLowerCase()));
+        }
     }
     if (cmd) {
         cmd.run(client, message, args)

c96642ad9157e19ecef48de58723ee7eb739b01c
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-05-13 12:17:17 +0300

    Add automated help command.

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.config = {
+    name: "help",
+    aliases: [],
+    usage: "Shows all bot commands."
+}
+
+exports.run = (client, message, args, commandsUsageList) => {
+    var helpmessage = []
+    commandsUsageList.forEach(helping => {
+        helpmessage.push(`**${helping.name}** => ${helping.usage}`)
+    });
+    var embed = new Discord.MessageEmbed()
+        .setTitle("Help")
+        .addField("Commands", helpmessage.join("\n"))
+    message.channel.send(embed)
+}

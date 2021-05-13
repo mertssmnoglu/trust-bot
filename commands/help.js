400ade0bef454f1497339df72ed8d56fdbffc491
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-05-13 12:29:41 +0300

    Add owneronly option.

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,8 @@ const Discord = require('discord.js');
 module.exports.config = {
     name: "help",
     aliases: [],
-    usage: "Shows all bot commands."
+    usage: "Shows all bot commands.",
+    owneronly: false
 }
 
 exports.run = (client, message, args, commandsUsageList) => {

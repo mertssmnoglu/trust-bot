7c45a340fb0ba6e19e7d6ecb38dbc3997c564b96
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-05-30 12:08:39 +0300

    Add 'Random Custom Status' option.

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,24 @@
 module.exports = (client) => {
+    var randomUserActivity = false
     var onlineMessage = `${client.user.username} is online!`
     console.log(onlineMessage);
-    client.user.setActivity(onlineMessage)
+    if (randomUserActivity) {
+
+        var randomTextes = [
+            "first text",
+            "second text",
+            "third text"
+        ]
+
+        const random = Math.floor(Math.random() * randomTextes.length);
+        client.user.setActivity(randomTextes[random])
+        setInterval(function () {
+            client.user.setActivity(randomTextes[random])
+        }, 20000);
+    } else {
+        client.user.setActivity(onlineMessage)
+        setInterval(function () {
+            client.user.setActivity(onlineMessage)
+        }, 7200000);
+    }
 }

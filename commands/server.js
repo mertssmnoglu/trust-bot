2cd7bf644e2d34e40acac9a6c3bb658d15ca6d32
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-01-12 23:48:52 +0300

    Add server.js to show server infos.

---
+++ commands/server.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+module.exports.config = {
+    name: "server",
+    aliases: ["server-info", "serverinfo", "guild", "guild-info", "guildinfo"],
+    usage: "Shows server info.",
+    owneronly: false
+}
+
+exports.run = (client, config, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle('Server Info')
+        .setThumbnail(message.guild.iconURL({
+            format: "png",
+            dynamic: true,
+            size: 4096
+        }))
+        .addField("Name:", message.guild.name, true)
+        .addField("Owner:", message.guild.owner, false)
+        .addField("Emojis Count:", message.guild.emojis.cache.size, true)
+        .addField("Roles Count:", `${message.guild.roles.cache.size}`, true)
+        .addField("Boost Level:", `${message.guild.premiumTier}`, true)
+        .addField("Boost Count:", `${message.guild.premiumSubscriptionCount}`, true)
+        .addField("Channel Count:", `${message.guild.channels.cache.size}`, true)
+        .addField("Member Count:", `${message.guild.memberCount}`, true)
+        .addField("✅ Online Members:", message.guild.members.cache.filter(member => member.presence.status !== "offline").size, true)
+        .addField("❌ Offline Members:", message.guild.members.cache.filter(member => member.presence.status == "offline").size, true)
+        .setTimestamp()
+        .setFooter(`Sunucu ID: ${message.guild.id}`);
+        message.channel.send(embed)
+}

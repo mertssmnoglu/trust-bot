24ed12af73edbbb590825974aea21cd451c6668d
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-01-14 00:23:20 +0300

    Fix member-members issue

--- commands/bancount.js
+++ commands/bancount.js
@@ -11,8 +11,11 @@ exports.run = (client, config, message, args) => {
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`:warning: You don't have enough permission to use this command!`)
     if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send(":warning: I can't see banned members. :cry:")
     message.guild.fetchBans().then(bans => {
-        memberormembers = "member"
-        if(bans.size > 1) memberormembers = "members"
+        if (bans.size > 1) {
+            var memberormembers = "members"
+        } else {
+            var memberormembers = "member"
+        }
         message.channel.send(`**${bans.size}** ${memberormembers} banned from this server.`)
     })
 }

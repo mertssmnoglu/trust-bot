831500f59c2f279156feeb851b5b29de13e7eb5d
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-01-13 23:47:10 +0300

    Add bancount command to see how man members or bots are banned.

---
+++ commands/bancount.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.config = {
+    name: "bancount",
+    aliases: ["ban-count"],
+    usage: "Ban count.",
+    owneronly: false
+}
+
+exports.run = (client, config, message, args) => {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`:warning: You don't have enough permission to use this command!`)
+    if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send(":warning: I can't see banned members. :cry:")
+    message.guild.fetchBans().then(bans => {
+        memberormembers = "member"
+        if(bans.size > 1) memberormembers = "members"
+        message.channel.send(`**${bans.size}** ${memberormembers} banned from this server.`)
+    })
+}
